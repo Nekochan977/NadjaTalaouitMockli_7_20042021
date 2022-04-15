1c1df29dff760d72a0402da9631f8aca48d6424f
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-04-15 12:07:43 +0200

    filter

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 "use strict";
-import recipes from "./recipes.js"
+import recipes from "./recipes.js";
 
 //variables that can be declared at top
 
 let ingredients = [];
 let appliances = [];
 let ustensils = [];
-const input = document.querySelector(".searchbar-input");
-const ingredientsSelect = document.querySelector(".ingredients-select");
-const applianceSelect = document.querySelector(".appliance-select");
-const ustensilsSelect = document.querySelector(".ustensils-select");
-
+let inputs = document.querySelectorAll(".input");
+let elements = document.querySelectorAll(".elements");
+const ingredientsSelect = document.querySelector(".ingredientElts");
+const applianceSelect = document.querySelector(".applianceElts");
+const ustensilsSelect = document.querySelector(".ustensilsElts");
 
 //------Getting the ingredients------
-recipes.map(recipe=>{
-    recipe.ingredients.map(allIngredients=>{
-        ingredients.push(allIngredients.ingredient.toLowerCase())
-    });
-    appliances.push(recipe.appliance.toLowerCase());
-    recipe.ustensils.map(allUstensils=>{
-        ustensils.push(allUstensils.toLowerCase());
-    })
-})
+recipes.forEach((recipe) => {
+  recipe.ingredients.forEach((allIngredients) => {
+    ingredients.push(allIngredients.ingredient.toLowerCase());
+  });
+  appliances.push(recipe.appliance.toLowerCase());
+  recipe.ustensils.forEach((allUstensils) => {
+    ustensils.push(allUstensils.toLowerCase());
+  });
+});
 // creating sets to get rid of duplicates
 const uniqueSet = new Set(ingredients);
 const applianceSet = new Set(appliances);
@@ -31,43 +31,63 @@ const ingredientList = [...uniqueSet];
 const applianceList = [...applianceSet];
 const ustensilsList = [...ustensilsSet];
 
+console.log(elements[1].getAttribute("appliances"));
+
 //Functions to add html nodes
-const displayIngredients =(ingredientList, node) => {
-    ingredientList.forEach(element => {
-        node.innerHTML += `<span class="ingredient-tag">${element}</span>&nbsp;`;
-    });
-}
-const displayAppliance =(applianceList, node) => {
-    applianceList.forEach(element => {
-        node.innerHTML += `<span class="ingredient-tag">${element}</span>&nbsp;`;
-    });
-}
+
+//turn 3 functions below to a single function using html data-attribute
+
+const displayIngredients = (ingredientList, node) => {
+  node.innerHTML = "";
+  ingredientList.forEach((element) => {
+    node.innerHTML += `<span class="ingredient-tag" data-name="ingredients">${element}</span>&nbsp;`;
+  });
+};
+const displayAppliance = (applianceList, node) => {
+  applianceList.forEach((element) => {
+    node.innerHTML += `<span class="appliance-tag" data-name="appliance">${element}</span>&nbsp;`;
+  });
+};
 const displayUstensils = (ustensilsList, node) => {
-    ustensilsList.forEach(element => {
-        node.innerHTML += `<span class="ingredient-tag">${element}</span>&nbsp;`;
-    })
-}
+  ustensilsList.forEach((element) => {
+    node.innerHTML += `<span class="ustensils-tag" data-name="ustensils">${element}</span>&nbsp;`;
+  });
+};
 
 //display the ingredients in html
 displayIngredients(ingredientList, ingredientsSelect);
 displayAppliance(applianceList, applianceSelect);
 displayUstensils(ustensilsList, ustensilsSelect);
 
-// console.log(ingredientList);
-// console.log(applianceList);
-// console.log(ustensilsList);
-//Event listener on input
-input.addEventListener("input", (e)=>{
-    //console.log(e.target.value);
-    const searchInput = e.target.value;
-    const filteredIngredients = ingredientList.filter((ingredient)=> {
-       for(let i = 0; i < ingredient.length; i++){
-        if(ingredient.includes(searchInput)) {
-            //console.log("toto");
-            return ingredient;
-        };
-    } 
+console.log(document.querySelectorAll(".ingredient-tag").forEach((tag)=>{
+    tag.getAttribute("ingredient")
+}));
+
+//Event listener on inputs //
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    // console.log(e.target.value);
+    let searchInput = e.target.value;
+    const filteredIngredients = ingredientList.filter((ingredient) => {
+      for (let i = 0; i < ingredient.length; i++) {
+        if (ingredient.includes(searchInput)) {
+          console.log("toto");
+          return true;
+        }
+        return false;
+      }
     });
-    console.log(filteredIngredients);
-    displayIngredients(filteredIngredients, ingredientsSelect)
-})
+    const filteredAppliances = applianceList.filter((appliance) => {
+        for (let i = 0; i < appliance.length; i++) {
+          if (appliance.includes(searchInput)) {
+            console.log("tata");
+            return true;
+          }
+          return false;
+        }
+      });
+    displayIngredients(filteredIngredients, ingredientsSelect);
+    displayIngredients(filteredAppliances, applianceList);
+  });
+});
+
